2c0f9eec6fcf987432dbd330ce642093cb1bdbcf
Miguel González <mgonzalez@codicesoftware.com>
2020-05-30 12:44:46 +0200

    Load the installed Bamboo plugin in the image

--- bamboo/server/plugins-entrypoint.sh
+++ bamboo/server/plugins-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cp ${BAMBOO_USER_HOME}/plugins/*.jar ${BAMBOO_INSTALL_DIR}/atlassian-bamboo/WEB-INF/lib
+if [ -f ${BAMBOO_USER_HOME}/plugins/bamboo-plasticscm-plugin-*.jar ]; then
+  cp ${BAMBOO_USER_HOME}/plugins/bamboo-plasticscm-plugin*.jar ${BAMBOO_INSTALL_DIR}/atlassian-bamboo/WEB-INF/lib
+fi
 
 exec /entrypoint.sh
